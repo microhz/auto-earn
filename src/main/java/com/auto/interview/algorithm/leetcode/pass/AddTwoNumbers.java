2d359571aebc20299c0fb311b128f87729b93389
jihai <pengmi2@126.com>
2020-08-06 16:33:27 +0800

    链表两个数字相加

--- src/main/java/com/auto/interview/algorithm/leetcode/pass/AddTwoNumbers.java
+++ src/main/java/com/auto/interview/algorithm/leetcode/pass/AddTwoNumbers.java
@@ -37,17 +37,77 @@ public class AddTwoNumbers {
 
     public static void main(String[] args) {
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+
+        ListNode listNode3 = ListNodeUtils.buildListNode(1, 3, 4);
+        ListNode listNode4 = ListNodeUtils.buildListNode(5, 6, 4);
+        ListNode resultListNode2 = addTwoNumbers.addTwoNumbers3(listNode3, listNode4);
+        AssertUtils.assertListNode(resultListNode2, 6, 9, 8);
         ListNode listNode = ListNodeUtils.buildListNode(2, 7, 3);
         ListNode listNode2 = ListNodeUtils.buildListNode(1, 6);
-        ListNode resultListNode = addTwoNumbers.addTwoNumbers(listNode, listNode2);
-        AssertUtils.assertListNode(resultListNode, 4, 3, 3);
+        ListNode resultListNode = addTwoNumbers.addTwoNumbers3(listNode, listNode2);
+        AssertUtils.assertListNode(resultListNode, 3, 3, 4);
 
 
-        ListNode listNode3 = ListNodeUtils.buildListNode(2, 4, 3);
-        ListNode listNode4 = ListNodeUtils.buildListNode(5, 6, 4);
-        ListNode resultListNode2 = addTwoNumbers.addTwoNumbers(listNode3, listNode4);
-        AssertUtils.assertListNode(resultListNode2, 8, 0, 7);
 
+        ListNode l1 = ListNodeUtils.buildListNode(2, 4, 3);
+        ListNode l2 = ListNodeUtils.buildListNode(5, 6, 4);
+        AssertUtils.assertListNode(addTwoNumbers.addTwoNumbers3(l1, l2), 7, 0, 8);
+    }
+
+
+    /**
+     * leetcode标准官方答案：
+     * 每个位加起来即可，但是遇到溢出需要处理，例如 8 + 9 = 17， 需要把这个1拿到下一个循环累计处理
+     * 在最终处理完了，这个溢出还有就再构造一个节点
+     */
+    public ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
+        // 初始化一个节点,最终返回的是next
+        ListNode resultList = new ListNode(0), index = resultList;
+
+        ListNode p = l1, q = l2;
+        int over = 0;
+        while (p != null || q != null) {
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+
+            int sum = x + y + over;
+            over = sum / 10;
+            index.next = new ListNode(sum % 10);
+            index = index.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (over > 0) {
+            index.next = new ListNode(over);
+        }
+        return resultList.next;
+    }
+
+
+    /**
+     * 别人的答案
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2)  {
+        ListNode dummyHead = new ListNode(-1), pre = dummyHead;
+        int t = 0;
+        while (l1 != null || l2 != null || t != 0) {
+            if (l1 != null) {
+                t += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                t += l2.val;
+                l2 = l2.next;
+            }
+            pre.next = new ListNode(t % 10);
+            pre = pre.next;
+            t /= 10;
+        }
+
+        return dummyHead.next;
     }
 
     /**
