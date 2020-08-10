e70f7295815e40aeff4d365d07ff186a8ea5f6cc
jihai <pengmi2@126.com>
2020-08-10 22:44:32 +0800

    排序测例

--- src/main/java/com/auto/interview/algorithm/leetcode/pass/sort/OrderAssert.java
+++ src/main/java/com/auto/interview/algorithm/leetcode/pass/sort/OrderAssert.java
@@ -1,6 +1,9 @@
 package com.auto.interview.algorithm.leetcode.pass.sort;
 
-import com.auto.interview.algorithm.leetcode.Assert;
+import com.auto.interview.algorithm.leetcode.AssertUtils;
+import com.google.common.collect.Lists;
+
+import java.util.List;
 
 /**
  * @author : jihai
@@ -10,31 +13,38 @@ import com.auto.interview.algorithm.leetcode.Assert;
 public class OrderAssert {
 
     public static void main(String[] args) {
+        List<Order> allOrderList = getAllOrderList();
+        allOrderList.forEach(e -> {
+            System.out.println("开始排序方式：" + e.getClass().getName());
+            assertOrderResult(e);
+        });
+    }
 
+    private static List<Order> getAllOrderList() {
+        return Lists.newArrayList(new BubbleOrder(),
+                new ChoiseOrder(),
+//                new CountSort(),
+                new InsertOrder(),
+                new MergeIntoSort(),
+                new QuickSort(),
+                new ShellSort());
+    }
+
+    private static void assertOrderResult(Order order) {
         int[] array = new int[]{5, 2, 3, 7, 1, 5, 10, 3, 4};
-//        Order bubbleOrder = new BubbleOrder();
-//        bubbleOrder.order(array);
-//        Assert.assertResult(() -> assertOrder(array, 1, 2, 3, 3, 4, 5, 5, 7, 10));
-
-//        Order choiseOrder = new ChoiseOrder();
-//        choiseOrder.order(array);
-//        Assert.assertResult(() -> assertOrder(array, 1, 2, 3, 3, 4, 5, 5, 7, 10));
-
-//        Order insertOrder = new InsertOrder();
-//        insertOrder.order(array);
-//        Assert.assertResult(() -> assertOrder(array, 1, 2, 3, 3, 4, 5, 5, 7, 10));
-
-//        Order shellSort = new ShellSort();
-//        shellSort.order(array);
-//        Assert.assertResult(() -> assertOrder(array, 1, 2, 3, 3, 4, 5, 5, 7, 10));
-
-        Order quickSort = new QuickSort();
         int[] array2 = new int[]{4, 7, 6, 5, 3, 2, 8, 1};
-        quickSort.order(array2);
-        Assert.assertResult(() -> assertOrder(array2, 1, 2, 3, 4, 5, 6, 7, 8));
-        quickSort.order(array);
-        Assert.assertResult(() -> assertOrder(array, 1, 2, 3, 3, 4, 5, 5, 7, 10));
+        int[] result = order.order(array2);
+        // case 1
+        AssertUtils.assertArray(result, 1, 2, 3, 4, 5, 6, 7, 8);
+        result = order.order(array);
+        // case 2
+        AssertUtils.assertArray(result, 1, 2, 3, 3, 4, 5, 5, 7, 10);
+        // case 3
+        AssertUtils.assertArray(order.order(new int[]{3, 2, 1, 3, 4, 5, -1}), -1, 1, 2, 3, 3, 4, 5);
+    }
 
+    private static Order getOrder() {
+        return new MergeIntoSort();
     }
 
     private static Boolean assertOrder(int[] array, int ... params) {
