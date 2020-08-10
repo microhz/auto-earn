86f6669ab3368ba6c612961d9bfa866517202966
jihai <pengmi2@126.com>
2020-08-10 23:19:14 +0800

    归并法链表排序

--- src/main/java/com/auto/interview/algorithm/leetcode/SortedList.java
+++ src/main/java/com/auto/interview/algorithm/leetcode/SortedList.java
@@ -17,14 +17,71 @@ package com.auto.interview.algorithm.leetcode;
  * 输入: -1->5->3->4->0
  * 输出: -1->0->3->4->5
  *
- *  * 输入: 4, 1, 2, 5, -2, -1, 0
- *   *  * 输入: 1->2->4->5->0->0->0
  */
 public class SortedList {
 
     public static void main(String[] args) {
         SortedList sortedList = new SortedList();
         AssertUtils.assertListNode(sortedList.sortList(ListNodeUtils.buildListNode(4, 1, 2, 5, -2, -1, 0)), -2,-1,0,1,2,4,5);
+    }
+
+
+    /**
+     * 双指针法，找到中间位置
+     * 切分成两个链表, 左链表排序， 右链表排序
+     * 最后归并
+     *
+     * 切分
+     * 排序
+     * 合并
+     *
+     * 时间复杂度 nLogn
+     * 空间复杂度 如果不考虑栈帧 1 复杂度
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null) return null;
+        // 双指针法找到中间点
+        ListNode pre = head;
+        if (pre.next == null) {
+            return pre;
+        }
+        ListNode p1 = pre, p2 = pre;
+        while (p2.next != null && p2.next.next != null) {
+            p2 = p2.next.next;
+            p1 = p1.next;
+        }
+        ListNode nextNode = p1.next;
+        p1.next = null;
+        ListNode leftNode = sortList(pre);
+        ListNode rightNode = sortList(nextNode);
+        return mergeList(leftNode, rightNode);
+    }
+
+
+    private ListNode mergeList(ListNode leftNode, ListNode rightNode) {
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        while (leftNode != null && rightNode != null) {
+            if (leftNode.val < rightNode.val) {
+                p.next = leftNode;
+                leftNode = leftNode.next;
+            } else {
+                p.next = rightNode;
+                rightNode = rightNode.next;
+            }
+            p = p.next;
+        }
+        while (leftNode != null) {
+            p.next = leftNode;
+            p = p.next;
+            leftNode = leftNode.next;
+        }
+        while (rightNode != null) {
+            p.next = rightNode;
+            p = p.next;
+            rightNode = rightNode.next;
+        }
+        return head.next;
     }
 
 
@@ -52,7 +109,7 @@ public class SortedList {
      * 取第一个节点，把链表小的移动到左边，大的移动到右边
      * 然后分别递归左边和右边
      */
-    public ListNode sortList(ListNode head) {
+    public ListNode sortList3(ListNode head) {
         ListNode min = head, p = head;
         while (p != null) {
             if (p.val < min.val) {
