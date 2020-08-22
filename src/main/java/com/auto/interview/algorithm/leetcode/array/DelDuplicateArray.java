3edfcb0386b78571f70a7a006279d88b5bc17374
jihai <pengmi2@126.com>
2020-08-22 18:21:47 +0800

    删除数组重复的元素双指针解法

--- src/main/java/com/auto/interview/algorithm/leetcode/array/DelDuplicateArray.java
+++ src/main/java/com/auto/interview/algorithm/leetcode/array/DelDuplicateArray.java
@@ -14,24 +14,27 @@ public class DelDuplicateArray {
         Assert.assertTrue(delDuplicateArray.removeDuplicates(new int[]{1, 1, 2}) == 2);
 
         // 0,0,1,1,1,2,2,3,3,4
-        Assert.assertTrue(delDuplicateArray.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}) == 2);
+        Assert.assertTrue(delDuplicateArray.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}) == 5);
+        Assert.assertTrue(delDuplicateArray.removeDuplicates2(new int[]{0,0,1,1,1,2,2,3,3,4}) == 5);
+
 
     }
 
+    /**
+     * 方法1：遇到重复的，一个一个移动重复数组
+     * 时间复杂度: n> && <n^2
+     * 空间复杂度 1
+     */
     // 因为是排序数组，所以累计每个数字重复，再用总数组-重复数字
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         int count = 0;
         for (int i = 0;i < nums.length - 1;) {
-            if (nums[i] == Integer.MAX_VALUE) {
-                break;
-            }
+            if (nums[i] == Integer.MAX_VALUE) break;
             if (nums[i] == nums[i + 1]) {
                 count ++;
                 for (int j = i; j < nums.length - 1; j++) {
-                    if (nums[j] != Integer.MAX_VALUE) {
-                        nums[j] = nums[j + 1];
-                    }
+                    if (nums[j] != Integer.MAX_VALUE) nums[j] = nums[j + 1];
                 }
                 nums[nums.length - 1] = Integer.MAX_VALUE;
             } else {
@@ -39,5 +42,21 @@ public class DelDuplicateArray {
             }
         }
         return nums.length - count;
+    }
+
+    /**
+     * 双指针!!
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                nums[++ i] = nums[j];
+            }
+        }
+        return i + 1;
     }
 }
