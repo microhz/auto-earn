20342fd1951fd6f4f6867c396bbed44c372c6e60
jihai <pengmi2@126.com>
2020-09-08 21:13:10 +0800

    自己写出来的一个牛逼的递归算法

---
+++ src/main/java/com/auto/interview/algorithm/leetcode/link/BinaryAdd.java
@@ -0,0 +1,88 @@
+package com.auto.interview.algorithm.leetcode.link;
+
+import com.auto.interview.algorithm.leetcode.base.ListNode;
+
+/**
+ * @author : jihai
+ * @date : 2020/9/8
+ * @description :
+ *
+ * 1290. 二进制链表转整数
+ * 给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+ *
+ * 请你返回该链表所表示数字的 十进制值 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [1,0,1]
+ * 输出：5
+ * 解释：二进制数 (101) 转化为十进制数 (5)
+ * 示例 2：
+ *
+ * 输入：head = [0]
+ * 输出：0
+ * 示例 3：
+ *
+ * 输入：head = [1]
+ * 输出：1
+ * 示例 4：
+ *
+ * 输入：head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+ * 输出：18880
+ * 示例 5：
+ *
+ * 输入：head = [0,0]
+ * 输出：0
+ *
+ *
+ * 提示：
+ *
+ * 链表不为空。
+ * 链表的结点总数不超过 30。
+ * 每个结点的值不是 0 就是 1。
+ */
+public class BinaryAdd {
+
+    public static void main(String[] args) {
+
+        System.out.println(1 << 0);
+        System.out.println(1 << 1);
+    }
+
+
+    /**
+     * 自己写的，迭代法
+     * @param head
+     * @return
+     */
+    public int getDecimalValue(ListNode head) {
+        double n = 0;
+        ListNode p = head;
+        while (p != null) {
+            n ++;
+            p = p.next;
+        }
+        double x = Math.pow(2D, n - 1);
+        int ret = 0;
+        p = head;
+        while (p != null) {
+            ret += p.val * x;
+            x /= 2;
+            p = p.next;
+        }
+        return ret;
+    }
+
+    /**
+     * 自己受启发写出来极简递归
+     */
+    int i = 0;
+    public int getDecimalValue2(ListNode head) {
+        if (head == null) return 0;
+        return getDecimalValue2(head.next) + head.val * (1 << i ++);
+    }
+}
