a675f67db43be0dc65e88e2ecb9b867c24f1e5a7
jihai <pengmi2@126.com>
2020-09-23 00:07:10 +0800

    很强的一个自底向上的递归算法-树节点的监控

--- src/main/java/com/auto/interview/algorithm/leetcode/tree/Camera.java
+++ src/main/java/com/auto/interview/algorithm/leetcode/tree/Camera.java
@@ -127,4 +127,49 @@ public class Camera {
         map.put(root, min);
         return min;
     }
+
+
+    /**
+     * 别人好的答案，值得学习
+     */
+
+    //NO_CAMERA表示的是子节点没有相机，当前节点也没放相机
+    private final int NO_CAMERA = 0;
+    //HAS_CAMERA表示当前节点有一个相机
+    private final int HAS_CAMERA = 1;
+    //NO_NEEDED表示当前节点没有相机，但他的子节点有一个相机，把它给
+    //覆盖了，所以它不需要了。或者他是一个空的节点也是不需要相机的
+    private final int NO_NEEDED = 2;
+
+    //全局的，统计有多少相机
+    int res = 0;
+
+    public int minCameraCover2(TreeNode root) {
+        //边界条件判断
+        if (root == null)
+            return 0;
+        //如果最后返回的是NO_CAMERA，表示root节点的子节点也没有相机，
+        //所以root节点要添加一个相机
+        if (dfs(root) == NO_CAMERA)
+            res++;
+        //返回结果
+        return res;
+    }
+
+    public int dfs(TreeNode root) {
+        //如果是空的，就不需要相机了
+        if (root == null)
+            return NO_NEEDED;
+        int left = dfs(root.left), right = dfs(root.right);
+        //如果左右子节点有一个是NO_CAMERA，表示的是子节点既没相机，也没相机覆盖它，
+        //所以当前节点需要有一个相机
+        if (left == NO_CAMERA || right == NO_CAMERA) {
+            //在当前节点放一个相机，统计相机的个数
+            res++;
+            return HAS_CAMERA;
+        }
+        //如果左右子节点只要有一个有相机，那么当前节点就不需要相机了，否则返回一个没有相机的标记
+        return left == HAS_CAMERA || right == HAS_CAMERA ? NO_NEEDED : NO_CAMERA;
+    }
+
 }
